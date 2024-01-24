2a7d4e7848add4c85a4db36903d02a8ff4b74ef7
Lashawn Butler <lashawnbutler50@gmail.com>
2024-01-24 16:56:38 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
+const multiply = 62;
+let num1 = 31;
+let num2 = 2;
+let product = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1
+let num3 = 68
+let num4 = 32
+const mod = num3 % num4
+console.log(mod)
+const max = Math.max(10, 20, 5, 15)
 // Write your code here
